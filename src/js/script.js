3d5bad39787941bdbedbad9911f7a1dcb9a3a687
Tarlan Kuliev <verge.beam@gmail.com>
2022-10-17 01:31:40 +0300

    Refactoring v.2

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
-let btns = document.querySelectorAll('button')
+let btns   = document.querySelectorAll('button'),
+    hidden = document.querySelector('.hidden')
 
 let mainLink    = 'https://jut.su/kimetsu-yaiba/season-1/',
     tanjiroLink = 'https://kimetsu-no-yaiba.fandom.com/wiki/Tanjiro_Kamado',
     nezukoLink  = 'https://kimetsu-no-yaiba.fandom.com/wiki/Nezuko_Kamado',
     zenitsuLink = 'https://kimetsu-no-yaiba.fandom.com/wiki/Zenitsu_Agatsuma',
-    inosukeLink = 'https://kimetsu-no-yaiba.fandom.com/wiki/Inosuke_Hashibira'
+    inosukeLink = 'https://kimetsu-no-yaiba.fandom.com/wiki/Inosuke_Hashibira',
+    link        = document.querySelector('link')
 
 for (let btn of btns) {
   btn.addEventListener('click', (e) => {
@@ -18,6 +20,12 @@ for (let btn of btns) {
       document.location = zenitsuLink
     }else if(btn.dataset.name === "inosuke") {
       document.location = inosukeLink
+    }else if(btn.dataset.name === "print") {
+      btn.dataset.name = "Noprint"
+      link.href = './src/css/print.css'
+    }else if(btn.dataset.name === "Noprint") {
+      btn.dataset.name = "print"
+      link.href = './src/css/style.css'
     }
   });
 }
